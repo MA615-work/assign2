34deb3563887271ab4052530053d784247da61c1
Ruining Jia <90628005+ruiningjia@users.noreply.github.com>
2021-10-11 06:35:22 +0800

    Add files via upload

--- wrangling_code.R
+++ wrangling_code.R
@@ -6,15 +6,19 @@ library(kableExtra)
 library(magrittr)
 # READ TABLE
 expectancy<-read.csv(file="life_expectancy_male.csv")
+cnames<-colnames(expectancy)[-1]
+cnames<-c("country",cnames)
+colnames(expectancy)<-cnames
 pl<-read.csv("population_aged_60plus_years_male_percent.csv")
+cnames<-colnames(pl)[-1]
+cnames<-c("country",cnames)
+colnames(pl)<-cnames
 # Tidy the datasetT he table below shows 10 randomly selected rows from the tidy
 expectancy_t<-expectancy%>% 
-  mutate_if(is.numeric,as.character, is.factor, as.character) %>%
-  pivot_longer(!country, names_to = "year", values_to = "count1")%>%
+  pivot_longer(!country, names_to = "year", values_to = "count1")
 
 pl_t<-pl%>% 
-  mutate_if(is.numeric,as.character, is.factor, as.character) %>%
-  pivot_longer(!country, names_to = "year", values_to = "count2")%>%
+  pivot_longer(!country, names_to = "year", values_to = "count2")
 # Wrangle these two data files into one tibble
 tibble=inner_join(x=expectancy_t,y=pl_t,by=c("country","year"))
 
